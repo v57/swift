a3638d47c2e1e2641ad15a5b3ee8852e960b52b1
Dmitry Kozlov <d@kozlov.ru>
2020-09-30 13:26:08 +0300

    Added more logs

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 cd ~
 
+echo Checking your ubuntu version
 vv=$(lsb_release -rs)
 case $vv in
 16.04)
 	os=ubuntu16.04
+  echo Found Ubuntu 16.04
 	;;
 18.04)
 	os=ubuntu18.04
+  echo Found Ubuntu 18.04
 	;;
 20.04)
 	os=ubuntu20.04
+  echo Found Ubuntu 20.04
 	;;
 *)
   echo Unsupported version.
@@ -44,20 +48,20 @@ read
 os2="${os//./}"
 
 # if version is already downloaded so we don't need to do anything
-if [ -e "swift/$os/$version.tar.gz" ]
+if [ -e "~/swift/$os/$version.tar.gz" ]
 then
   echo "Using local archive"
 else
   # installing dependencies
   apt-get install clang libicu-dev libcurl4-openssl-dev libpython2.7 pkg-config uuid-dev openssl libssl-dev -y
   # creating directory
-  mkdir -p "swift/$os"
+  mkdir -p "~/swift/$os"
   # downloading swift archive
-  wget -O "swift/$os/$version.tar.gz" "https://swift.org/builds/swift-$version-release/$os2/swift-$version-RELEASE/swift-$version-RELEASE-$os.tar.gz"
+  wget -O "~/swift/$os/$version.tar.gz" "https://swift.org/builds/swift-$version-release/$os2/swift-$version-RELEASE/swift-$version-RELEASE-$os.tar.gz"
   # extracting swift archive
-  tar xzf "swift/$os/$version.tar.gz"
+  tar xzf "~/swift/$os/$version.tar.gz"
 fi
 # moving swift archive to /usr directory
-rsync -avh "swift/$os/$version/usr" /
+rsync -avh "~/swift/$os/$version/usr" /
 # printing swift version
 swift --version
